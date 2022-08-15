c95fd73a498ecd2e954c629d6849cf7cbd3921ad
Kryspin Sarosiek <sarosiek@icloud.com>
2022-08-15 16:49:55 +0100

    WIP

---
+++ TryDo/TryDo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TryDo
+//
+//  Created by Kryspin Sarosiek on 15/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
